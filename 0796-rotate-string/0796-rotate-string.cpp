f89dd639228d0de05c65e1b974c18d997101d867
Ayush anand <167104076+Ayushshiv07@users.noreply.github.com>
2026-05-03 05:57:26 +0530

    Time: 0 ms (100%), Space: 8.2 MB (61.66%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size() != goal.size()) return false;
+        string newS = s + s;
+        return(newS.find(goal) != string::npos);
+    }
+};
